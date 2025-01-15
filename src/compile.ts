059cb4bd82ca4bdef43ded1acca9ed56f560fb9b
Masaki Kobayashi <contact@mkobayashi.me>
2025-01-15 18:16:26 +0900

    chore: format

--- src/compile.ts
+++ src/compile.ts
@@ -2,24 +2,24 @@ import * as esbuild from "esbuild";
 import { minify } from "terser";
 
 export const compile = async (filename: string) => {
-  const esbuildOutput = await esbuild.build({
-    entryPoints: [filename],
-    bundle: true,
-    minify: true,
-    format: "esm",
-    write: false,
-  });
+	const esbuildOutput = await esbuild.build({
+		entryPoints: [filename],
+		bundle: true,
+		minify: true,
+		format: "esm",
+		write: false,
+	});
 
-  if (!esbuildOutput.outputFiles[0]) {
-    throw new Error("esbuild outputFiles is empty");
-  }
-  const code = esbuildOutput.outputFiles[0].text;
+	if (!esbuildOutput.outputFiles[0]) {
+		throw new Error("esbuild outputFiles is empty");
+	}
+	const code = esbuildOutput.outputFiles[0].text;
 
-  const prod = `javascript:(()=>{${encodeURIComponent((await minify(code)).code ?? "")}})()`;
-  const dev = prod.slice(1);
+	const prod = `javascript:(()=>{${encodeURIComponent((await minify(code)).code ?? "")}})()`;
+	const dev = prod.slice(1);
 
-  return {
-    prod,
-    dev,
-  };
+	return {
+		prod,
+		dev,
+	};
 };
